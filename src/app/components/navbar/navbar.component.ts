69d8de6342338b735f1594bde2607631807ec894
Piotr Kalota <piotrkalota99@gmail.com>
2021-09-30 18:19:36 +0200

    Add menu show animation on click

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  @ViewChild('menuRef')
+  menuRef!: ElementRef;
+
+  menuClass = 'menu-closed';
+  menuOpened = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleMenu(): void {
+    if (this.menuOpened)
+      this.menuClass = 'menu-closed';
+    else
+      this.menuClass = 'menu-opened';
+    this.menuOpened = !this.menuOpened;
+  }
 }
