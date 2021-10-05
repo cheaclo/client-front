686a435ac7852c380510b694c810c11cc8281dd4
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-05 11:49:56 +0200

    Add draft of shop slider

--- src/app/components/home/shop-search/shop-search.component.ts
+++ src/app/components/home/shop-search/shop-search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Shop } from 'src/app/models/shop';
 
 @Component({
   selector: 'app-shop-search',
@@ -6,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop-search.component.scss']
 })
 export class ShopSearchComponent implements OnInit {
+  PATH_TO_IMAGES = 'assets/images/';
+  IMAGES_EXT = '.png';
+  shops: Shop[] = [];
 
-  constructor() { }
+  //TODO: replace this list with service incjection
+  tempNames: string[] = ['hm', 'ca', 'reserved'];
+
+  constructor() {
+    for (let name of this.tempNames) {
+      let imageSource = this.PATH_TO_IMAGES + name + this.IMAGES_EXT;
+      this.shops.push({name, imageSource});
+    }
+  }
 
   ngOnInit(): void {
   }
