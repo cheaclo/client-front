6d97e8dbf03cbc45305383c03b764c3c8ee32fa7
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-07 14:17:41 +0200

    Add sign in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { SingInComponent } from './components/sing-in/sing-in.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
@@ -6,7 +7,8 @@ import { SearchComponent } from './components/search/search.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'search', component: SearchComponent }
+  { path: 'search', component: SearchComponent },
+  { path: 'sign-in', component: SingInComponent }
 ];
 
 @NgModule({
