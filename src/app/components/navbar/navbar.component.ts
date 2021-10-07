2a953ba2b3ba9e0728b24e475ed94df68466ef17
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-07 23:22:51 +0200

    Add show menu in case of being logged

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -16,6 +16,13 @@ export class NavbarComponent implements OnInit {
   }
 
   toggleMenu(): void {
-    this.menuOpened = !this.menuOpened;
+    if (this.isUserLogged()) {
+      this.menuOpened = !this.menuOpened;
+    }
+  }
+
+  isUserLogged() {
+    let userLogged = sessionStorage.getItem('userLogged');
+    return userLogged == 'true';
   }
 }
