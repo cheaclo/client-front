0a587348644ef06138cf8fd82382af43e549aba8
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-13 23:08:35 +0200

    Add draft of product implementation

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductResponse } from 'src/app/models/productResponse';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  @Input() product!: ProductResponse;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngRedirectToProduct(): void {
+    window.open(
+      this.product.details.productUrl,
+      '_blank'
+    );
+  }
 }
--- src/app/components/products-by-type/products-by-type.component.ts
+++ src/app/components/products-by-type/products-by-type.component.ts
@@ -2,6 +2,7 @@ import { CategoryResponse } from './../../models/categoryResponse';
 import { CategoryService } from './../../services/category.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProductResponse } from 'src/app/models/productResponse';
 
 @Component({
   selector: 'app-products-by-type',
@@ -10,10 +11,35 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProductsByTypeComponent implements OnInit {
   categories: CategoryResponse[] = [];
+  products: ProductResponse[] = [
+    {
+      id: 1,
+      details: {
+        title: 'title',
+        price: 23.5,
+        regularPrice: 44.2,
+        productUrl: 'https://www.c-and-a.com/eu/en/shop/sleeveless-blouse-2150336/2?categoryId=16199',
+        imageUrl: 'https://www.c-and-a.com/productimages/b_rgb:ECECEC,c_scale,h_430,q_auto:eco,e_sharpen:70/v1626777793/2150336-2-01.jpg',
+      },
+      categories: {
+        id: 1,
+        name: 'Accessories',
+      },
+      shop: {
+        id: 1,
+        name: 'HM',
+      },
+      type: {
+        id: 1,
+        name: 'man',
+      },
+      lastUpdate: new Date(),
+      hash: 2352352
+    }
+  ];
 
   constructor(private categoryService: CategoryService,
-              private route: ActivatedRoute,
-              private router: Router) {
+              private route: ActivatedRoute) {
 
     this.route.params.subscribe(params => {
       const type = params['type'];
