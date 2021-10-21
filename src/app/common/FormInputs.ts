8cefa6d40139e70ee43a76fe98869fb7a49d58fd
Piotr Kalota <piotrkalota99@gmail.com>
2021-10-21 14:22:53 +0200

    Add form inputs class

---
+++ src/app/common/FormInputs.ts
@@ -0,0 +1,151 @@
+import { FormInput } from "../models/formInput";
+
+export class FormInputs {
+  EMAIL_REGEX = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+  CITY_REGEX = new RegExp(/^\w+[\s*\w*]*$/);
+  POSTAL_CODE_REGEX = new RegExp(/^\w{6}$/);
+
+  showExtraInfo = false;
+
+  firstname: FormInput = {
+    errorMessage: "Invalid firstname",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: any) => {return val === undefined || !(val.length >= 2 && val.length <= 30)}
+  };
+  lastname: FormInput = {
+    errorMessage: "Invalid lastname",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return val === undefined || !(val.length >= 2 && val.length <= 30)}
+  };
+  email: FormInput = {
+    errorMessage: "Invalid email",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate:  (val: string) => {return val === undefined || !this.EMAIL_REGEX.test(val)}
+  };
+  password: FormInput = {
+    errorMessage: "Allowed length between 5 and 30",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return val === undefined || !(val.length >= 5 && val.length <= 30)}
+  };
+  repeatPassword: FormInput = {
+    errorMessage: "Passwords have to be the same",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return val === undefined || val !== this.password.value}
+  };
+  country: FormInput = {
+    errorMessage: "Invalid country",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === 'Country')}
+  };
+  phoneNumber: FormInput = {
+    errorMessage: "Ivalid phone number",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val.length !== 9)}
+  };
+  street: FormInput = {
+    errorMessage: "Invalid street name",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !(val.length >= 3 && val.length <= 50))}
+  };
+  streetNumber: FormInput = {
+    errorMessage: "Allowed number between 1 and 99999",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || Number(val) === NaN || !(Number(val) >= 1 && Number(val) <= 99999))}
+  };
+  city: FormInput = {
+    errorMessage: "Invalid city",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.CITY_REGEX.test(val))}
+  };
+  postalCode: FormInput = {
+    errorMessage: "Invalid postal code",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.POSTAL_CODE_REGEX.test(val))}
+  };
+  birthday: FormInput = {
+    errorMessage: "Invalid birthday",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {
+      let date = new Date(val);
+      return this.showExtraInfo == true && (val === undefined || isNaN(Date.parse(val)) || date > new Date());
+    }
+  };
+  gender: FormInput = {
+    errorMessage: "Invalid gender",
+    currentMessage: "",
+    valid: true,
+    value: "",
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === "")}
+  };
+
+  formFields: FormInput[] = [
+    this.firstname,
+    this.lastname,
+    this.email,
+    this.password,
+    this.repeatPassword,
+    this.country,
+    this.phoneNumber,
+    this.street,
+    this.streetNumber,
+    this.city,
+    this.postalCode,
+    this.birthday,
+    this.gender
+  ]
+
+  validFields(): boolean {
+    let valid = true;
+
+    for (let input of this.formFields) {
+      if (input.validate(input.value)) {
+        input.valid = false;
+        input.currentMessage = input.errorMessage;
+        valid = false;
+      }
+    }
+
+    return valid;
+  }
+
+  getSingUpRequestBody(): any {
+    return {
+      firstname: this.firstname.value,
+      lastname: this.lastname.value,
+      email: this.email.value,
+      password: btoa(this.password.value),
+      birthday: this.birthday.value,
+      gender: this.gender.value === "" ? null : this.gender.value,
+      phone: this.phoneNumber.value,
+      street: this.street.value,
+      streetNumber: this.streetNumber.value,
+      city: this.city.value,
+      postalCode: this.postalCode.value,
+      country: this.country.value
+    }
+  }
+}
