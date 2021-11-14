9399622f3bde3864ad38e8988a2dcea5cb17f8db
Piotr Kalota <piotrkalota99@gmail.com>
2021-11-14 19:26:53 +0100

    Remove test logged user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  temp = '{"id":6,"createDate":"2021-09-23T16:51:03.451+00:00","accountInfo":{"id":6,"firstname":"Piotr","lastname":"Kalota","email":"random@gmail.com","password":"cGFzc3dvcmQ=","birthdayDate":"1999-04-23T00:00:00.000+00:00","gender":"MAN","phone":"222333111","address":{"id":6,"street":"Cambell","streetNumber":57,"city":"York City","postalCode":"Y010ET","country":"United Kingdom"}}}';
+  // testUser = '{"id":6,"createDate":"2021-09-23T16:51:03.451+00:00","accountInfo":{"id":6,"firstname":"Piotr","lastname":"Kalota","email":"random@gmail.com","password":"cGFzc3dvcmQ=","birthdayDate":"1999-04-23T00:00:00.000+00:00","gender":"MAN","phone":"222333111","address":{"id":6,"street":"Cambell","streetNumber":57,"city":"York City","postalCode":"Y010ET","country":"United Kingdom"}}}';
 
   ngOnInit(): void {
-    // temp
-    sessionStorage.setItem('userLogged', 'true');
-    sessionStorage.setItem('user', this.temp);
+    sessionStorage.setItem('userLogged', 'false');
+    sessionStorage.setItem('user', '{}');
     sessionStorage.setItem('cart', JSON.stringify([]));
   }
 }
