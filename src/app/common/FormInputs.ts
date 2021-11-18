eea7046cc3325e755a897cd035fd84dd7afd5d84
Piotr Kalota <piotrkalota99@gmail.com>
2021-11-19 00:05:05 +0100

    Add require field to FormInputs

--- src/app/common/FormInputs.ts
+++ src/app/common/FormInputs.ts
@@ -14,77 +14,88 @@ export class FormInputs {
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: any) => {return val === undefined || !(val.length >= 2 && val.length <= 30)}
+    validate: (val: any) => {return val === undefined || !(val.length >= 2 && val.length <= 30)},
+    required: true
   };
   lastname: FormInput = {
     errorMessage: "Invalid lastname",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return val === undefined || !(val.length >= 2 && val.length <= 30)}
+    validate: (val: string) => {return val === undefined || !(val.length >= 2 && val.length <= 30)},
+    required: true
   };
   email: FormInput = {
     errorMessage: "Invalid email",
     currentMessage: "",
     valid: true,
     value: "",
-    validate:  (val: string) => {return val === undefined || !this.EMAIL_REGEX.test(val)}
+    validate:  (val: string) => {return val === undefined || !this.EMAIL_REGEX.test(val)},
+    required: true
   };
   password: FormInput = {
     errorMessage: "Allowed length between 5 and 30",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.checkPassword && (val === undefined || !(val.length >= 5 && val.length <= 30))}
+    validate: (val: string) => {return this.checkPassword && (val === undefined || !(val.length >= 5 && val.length <= 30))},
+    required: true
   };
   repeatPassword: FormInput = {
     errorMessage: "Passwords have to be the same",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.checkPassword && (val === undefined || val !== this.password.value)}
+    validate: (val: string) => {return this.checkPassword && (val === undefined || val !== this.password.value)},
+    required: true
   };
   country: FormInput = {
     errorMessage: "Invalid country",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === 'Country')}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === 'Country')},
+    required: false
   };
   phoneNumber: FormInput = {
     errorMessage: "Ivalid phone number",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val.length !== 9)}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val.length !== 9)},
+    required: false
   };
   street: FormInput = {
     errorMessage: "Invalid street name",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !(val.length >= 3 && val.length <= 50))}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !(val.length >= 3 && val.length <= 50))},
+    required: false
   };
   streetNumber: FormInput = {
     errorMessage: "Allowed number between 1 and 99999",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || Number(val) === NaN || !(Number(val) >= 1 && Number(val) <= 99999))}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || Number(val) === NaN || !(Number(val) >= 1 && Number(val) <= 99999))},
+    required: false
   };
   city: FormInput = {
     errorMessage: "Invalid city",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.CITY_REGEX.test(val))}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.CITY_REGEX.test(val))},
+    required: false
   };
   postalCode: FormInput = {
     errorMessage: "Invalid postal code",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.POSTAL_CODE_REGEX.test(val))}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || !this.POSTAL_CODE_REGEX.test(val))},
+    required: false
   };
   birthday: FormInput = {
     errorMessage: "Invalid birthday",
@@ -94,14 +105,16 @@ export class FormInputs {
     validate: (val: string) => {
       let date = new Date(val);
       return this.showExtraInfo == true && (val === undefined || isNaN(Date.parse(val)) || date > new Date());
-    }
+    },
+    required: false
   };
   gender: FormInput = {
     errorMessage: "Invalid gender",
     currentMessage: "",
     valid: true,
     value: "",
-    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === "")}
+    validate: (val: string) => {return this.showExtraInfo == true && (val === undefined || val === "")},
+    required: false
   };
 
   formFields: FormInput[] = [
--- src/app/models/formInput.ts
+++ src/app/models/formInput.ts
@@ -4,4 +4,5 @@ export interface FormInput {
   valid: boolean;
   validate: any;
   value: any;
+  required: boolean;
 }
