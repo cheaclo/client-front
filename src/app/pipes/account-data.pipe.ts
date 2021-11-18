00dd48d6e7a5ed976378c727c71813b3648a86bb
Piotr Kalota <piotrkalota99@gmail.com>
2021-11-19 00:31:56 +0100

    Remove console.log from account data pipe

--- src/app/pipes/account-data.pipe.ts
+++ src/app/pipes/account-data.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AccountDataPipe implements PipeTransform {
 
   transform(value: any): unknown {
-    console.log(value);
     if (typeof (value === Date)) {
       return value.toString().slice(0, 10);
     } else {
